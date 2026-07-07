4b171a39b4ae15c1189e403e98231588702c58fe
chrischall <chris.c.hall@gmail.com>
2026-07-07 17:08:28 +0200

    chore(main): release 1.1.0 (#19)

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import { registerHealthcheckTools } from './tools/healthcheck.js';
 // always succeeds before any credential check runs.
 await runMcp({
   name: 'alltrails',
-  version: '1.0.0', // x-release-please-version
+  version: '1.1.0', // x-release-please-version
   deps: client,
   tools: [registerTrailTools, registerExploreTools, registerUserTools, registerHealthcheckTools],
   banner:
